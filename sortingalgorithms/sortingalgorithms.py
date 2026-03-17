7599e8514e340671960fe9b077fe675a03d68b50
taniasenpai <tania.ungureanu@e-uvt.ro>
2026-03-17 14:24:34 +0200

    timtim

--- sortingalgorithms/sortingalgorithms.py
+++ sortingalgorithms/sortingalgorithms.py
@@ -75,9 +75,6 @@ def quick_sort(arr):
     return quick_sort(left) + middle + quick_sort(right)
 
 
-def python_sort(arr):
-    return sorted(arr)
-
 def merge_three(arr, left, mid1, mid2, right):
     left_arr = arr[left:mid1 + 1]
     mid_arr = arr[mid1 + 1:mid2 + 1]
@@ -278,6 +275,99 @@ def bucket_sort(arr):
 
     return result
 
+minRUN = 32
+def calcMinRun(n):
+    r = 0
+    while n >= minRUN:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+def insertionSort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def merge1(arr, l, m, r):
+    left = arr[l:m+1]
+    right = arr[m+1:r+1]
+    i = j = 0
+    k = l
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+def findRun(arr, start, n):
+    end = start + 1
+    if end == n: return end
+    if arr[end] < arr[start]:
+        # descending
+        while end < n and arr[end] < arr[end - 1]:
+            end += 1
+        arr[start:end] = reversed(arr[start:end])
+    else:
+        # ascending
+        while end < n and arr[end] >= arr[end - 1]:
+            end += 1
+    return end
+
+def timsort(arr):
+    n = len(arr)
+    minRun = calcMinRun(n)
+    runs = []
+
+    i = 0
+    while i < n:
+        runEnd = findRun(arr, i, n)
+        runLen = runEnd - i
+
+        if runLen < minRun:
+            end = min(i + minRun, n)
+            insertionSort(arr, i, end - 1)
+            runEnd = end
+
+        runs.append((i, runEnd))
+        i = runEnd
+
+        while len(runs) > 1:
+            l1, r1 = runs[-2]
+            l2, r2 = runs[-1]
+            len1, len2 = r1 - l1, r2 - l2
+            if len1 <= len2:
+                merge1(arr, l1, r1 - 1, r2 - 1)
+                runs.pop()
+                runs[-1] = (l1, r2)
+            else:
+                break
+
+    while len(runs) > 1:
+        l1, r1 = runs[-2]
+        l2, r2 = runs[-1]
+        merge1(arr, l1, r1 - 1, r2 - 1)
+        runs.pop()
+        runs[-1] = (l1, r2)
+
+        return arr;
+
+
+
 
 # -------------------------------
 # Data Generators
@@ -334,7 +424,7 @@ sorting_algorithms = {
     "Counting": counting_sort,
     "Radix": radix_sort,
     "Bucket": bucket_sort,
-    "Python": python_sort
+    "Timsort": timsort
 }
 
 data_types = {
